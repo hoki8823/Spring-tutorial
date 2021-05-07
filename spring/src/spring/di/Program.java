7d973d18a17cc48e9e4b84f8ad5c20c5f28df2df
hoki8823 <hoki8823@naver.com>
2021-05-07 21:44:21 +0900

    @Autowired,@Qualifier,@Autowired(required = false) 숙지

--- spring/src/spring/di/Program.java
+++ spring/src/spring/di/Program.java
@@ -1,10 +1,14 @@
 package spring.di;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import spring.di.entity.Exam;
 import spring.di.entity.NewlecExam;
 import spring.di.ui.ExamConsole;
-import spring.di.ui.GridExamConsole;
-import spring.di.ui.InlineExamConsole;
 
 public class Program {
 
@@ -15,11 +19,24 @@ public class Program {
 		ExamConsole console = new GridExamConsole();
 		
 		console.setExam(exam);
+		
 		*/
 		
-
-		ExamConsole console = ?;
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring/di/setting.xml");
+		
+		//Exam exam = context.getBean(Exam.class);
+		//System.out.println(exam.toString());
+		
+		ExamConsole console = (ExamConsole) context.getBean("console");
+		//ExamConsole console = context.getBean(ExamConsole.class);
 		console.print();
+		
+		//List<Exam> exams = (List<Exam>)context.getBean("exams");//new ArrayList<>();
+		//exams.add(new NewlecExam(1,1,1,1));
+		
+		//for(Exam e : exams)
+		//	System.out.println(e);
+		
 		
 	}
 	
--- spring/src/spring/di/entity/NewlecExam.java
+++ spring/src/spring/di/entity/NewlecExam.java
@@ -7,6 +7,51 @@ public class NewlecExam implements Exam {
 	private int math;
 	private int com;
 	
+	//생성자
+	public NewlecExam() {
+		// TODO Auto-generated constructor stub
+	}
+	//오버로드
+	public NewlecExam(int kor, int eng, int math, int com) {
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.com = com;
+	}
+
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	public int getCom() {
+		return com;
+	}
+
+	public void setCom(int com) {
+		this.com = com;
+	}
+
 	@Override
 	public int total() {
 		return kor+eng+math+com;
@@ -15,6 +60,10 @@ public class NewlecExam implements Exam {
 	@Override
 	public float avg() {
 		return total() / 4.0f;
+	}
+	@Override
+	public String toString() {
+		return "NewlecExam [kor=" + kor + ", eng=" + eng + ", math=" + math + ", com=" + com + "]";
 	}
 
 }
