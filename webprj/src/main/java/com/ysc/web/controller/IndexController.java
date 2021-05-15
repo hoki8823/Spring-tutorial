e17e2c262110052ed1919bbfc1f97fb301a0d80e
hoki8823 <hoki8823@naver.com>
2021-05-15 15:26:37 +0900

    Spring MVC (스프링 웹 MVC) 강의 19 - Root 페이지들을 위한 Layout 페이지 만들기

--- webprj/src/main/java/com/ysc/web/controller/IndexController.java
+++ webprj/src/main/java/com/ysc/web/controller/IndexController.java
@@ -10,7 +10,7 @@ public class IndexController implements Controller{
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		ModelAndView mv = new ModelAndView("index");
+		ModelAndView mv = new ModelAndView("root.index");
 		mv.addObject("data","에이치 아이 하이~");
 		//mv.setViewName("/WEB-INF/view/index.jsp");
 		return mv;
